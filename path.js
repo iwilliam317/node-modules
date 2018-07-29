b8cf71ed3234def32a3b9d55160ddbb592aa2a9b
william <willshinji@gmail.com>
2018-07-29 19:18:22 -0300

    added more methods

--- path.js
+++ path.js
@@ -1,5 +1,17 @@
 const path = require('path');
 
-console.log(`Last part of a path: __ ${path.basename(__dirname)}`);
+// basename
+// Returns the last part of a path
+console.log(`${path.basename(__dirname)}`);
 
-console.log(`Current path: __ ${__dirname}`);
+// dirname
+// Current path
+console.log(`${__dirname}`);
+
+// resolve
+// Resolves the specified paths into an absolute path
+console.log(`${path.resolve(__dirname)}`);
+
+// join
+// Joins the specified paths into one
+console.log(`${path.join('Users', 'Refsnes', 'demo_path.js')}`);
