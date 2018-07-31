908c06d6b5da7022d043ef12790e7c217d27e5e4
william <willshinji@gmail.com>
2018-07-30 22:45:52 -0300

    adds comments

--- fs.js
+++ fs.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 
-// appendFile
+// appendFile()
 // Create a new file using the appendFile() method
 fs.appendFile(path.join(__dirname, 'fs', 'mynewfile1.txt'), 'Hello content!',  err => {
   if (err) throw err;
--- path.js
+++ path.js
@@ -1,21 +1,21 @@
 const path = require('path');
 
-// basename
+// basename()
 // Returns the last part of a path
 console.log(`${path.basename(__dirname)}`);
 
-// dirname
+// dirname()
 // Current path
 console.log(`${__dirname}`);
 
-// resolve
+// resolve()
 // Resolves the specified paths into an absolute path
 console.log(`${path.resolve('public')}`);
 
-// join
+// join()
 // Joins the specified paths into one
 console.log(`${path.join(__dirname, 'public', 'views')}`);
 
-//extname
+//extname()
 //Returns the file extension of a path
 console.log(path.extname('index.js'));
