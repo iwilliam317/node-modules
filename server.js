f29467f6c4c772017380c071eda9c3ab89dac075
william <willshinji@gmail.com>
2018-08-23 00:04:31 -0300

    set template engine ejs

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express');
 
 const app = express();
 
+app.set('view_engine', 'ejs');
+app.set('views', './views');
+
 app.listen(3000, () => console.log('Server up!'));
 
 app.get('/', (request, response) => response.send('Hello World!'));
