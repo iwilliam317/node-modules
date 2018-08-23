d5de94031c78e18625aaac2e692b63dda336eb28
william <willshinji@gmail.com>
2018-08-23 00:07:19 -0300

    express-validator form created

--- express_validator.js
+++ express_validator.js
@@ -1,6 +1,10 @@
 const app = require('./server');
 const expressValidator = require('express-validator');
 
+app.get('/form', (request, response) => {
+  response.render('form');
+});
+
 app.post('/save', (request, response) => {
   response.send('asdiasjdiask');
 })
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-app.set('view_engine', 'ejs');
+app.set('view engine', 'ejs');
 app.set('views', './views');
 
 app.listen(3000, () => console.log('Server up!'));
