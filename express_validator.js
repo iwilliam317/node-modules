ac647d281fdcdb25a51c331bff605da0e0a69afe
william <willshinji@gmail.com>
2018-08-24 00:24:42 -0300

    update express-validator example

--- express_validator.js
+++ express_validator.js
@@ -1,11 +1,17 @@
 const app = require('./server');
-const expressValidator = require('express-validator');
 
 app.get('/form', (request, response) => {
   response.render('form');
 });
 
 app.post('/save', (request, response) => {
-  response.send('asdiasjdiask');
+    request.assert('name', 'Name can\'t be blank!').notEmpty();
+    let error = request.validationErrors();
+    console.log(error)
+    if (error){      
+      return response.render('form', { erros: erros})
+    }
+    
+
 })
 
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 
+const expressValidator = require('express-validator');
+
 const bodyParser = require('body-parser');
 
 const app = express();
