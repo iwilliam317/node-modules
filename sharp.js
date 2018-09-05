637e9daaab54967fe103409be76052652be67038
william <willshinji@gmail.com>
2018-09-05 12:09:05 -0300

    add image-processing example with sharp

---
+++ sharp.js
@@ -0,0 +1,14 @@
+const sharp = require('sharp');
+const path = require('path');
+
+const uploadDirectory = path.join(__dirname, 'uploads');
+
+sharp(`${uploadDirectory}/image.jpg`)
+  .resize(50, 50)
+  .rotate(180)
+  .toFile(`${uploadDirectory}/image_50_50.jpg`, (error, info) => {
+
+    if (error) throw error;
+
+    console.log(info);
+  });
