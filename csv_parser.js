5395f76826fab2f5c6621a0549c27a2e9b5446f8
william <willshinji@gmail.com>
2018-09-06 15:00:11 -0300

    add csv-parser example

---
+++ csv_parser.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+
+const csv = require('csv-parser');
+
+fs.createReadStream('./files/names.csv')
+  .pipe(csv())
+  .on('data', function (data) {
+    console.log('Name: %s Age: %s', data.NAME, data.AGE)
+  })
